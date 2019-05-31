fcc3703192c1775b16547154ceb44b4106dfb153
Gopala Krishnan Srinivasan <gsrinivasan@ztech.io>
2019-06-01 04:47:27 +0530

    removed cache policy

--- src/services/project.js
+++ src/services/project.js
@@ -10,7 +10,7 @@ export const getIntrospectionSchema = (endPoint) => {
         variables: {
             endPoint: endPoint
         },
-        fetchPolicy: "network-only"
+        fetchPolicy: "cache-first"
     })
         .then((response) => JSON.parse(response.data.getIntrospectionSchema).data);
 }
