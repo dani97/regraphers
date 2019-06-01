48bccc3eb54f4fb788646617ad8c36325ae8232f
Christopher Daniel <cdaniel@ztech.io>
2019-06-02 01:56:57 +0530

    model triggering corrected

--- src/components/QueryGrid.js
+++ src/components/QueryGrid.js
@@ -11,14 +11,18 @@ import QueryTester from "./QueryTester";
 const QueryGrid = (props) => {
 
     let [queries, setQueries] = useState([]);
-    let [visible, setVisible] = useState(false);
+    let [visible, setVisible] = useState([]);
 
-    const onOpenModal = () => {
-        setVisible(true);
+    const onOpenModal = (id) => {
+        let visibleState = visible.slice(0);
+        visibleState[id] = true;
+        setVisible(visibleState);
     }
 
-    const onCloseModal= () => {
-        setVisible(false);
+    const onCloseModal= (id) => {
+        let visibleState = visible.slice(0);
+        visibleState[id] = false;
+        setVisible(visibleState);
     }
 
     console.log('in query gris props ', props);
@@ -33,6 +37,7 @@ const QueryGrid = (props) => {
                     if(endPointOperations && endPointOperations.items && endPointOperations.items.length) {
                         console.log('before setting to query  ', endPointOperations.items);
                         setQueries(endPointOperations.items);
+                        setVisible(Array(endPointOperations.items.length).fill(false));
                     }
                 }
             });
@@ -43,16 +48,16 @@ const QueryGrid = (props) => {
         <div>
             {queries.map((query, index) => (
                 <div key={index}>
-                    <div onClick={onOpenModal}>
+                    <div onClick={() => onOpenModal(index)}>
                         <div>
                             <h3>{query.name}</h3>
                             <p>{query.description}</p>
                         </div>
                     </div>
-                    <Modal visible={visible} width="1000" height="550" effect="fadeInUp" onClickAway={onCloseModal}>
+                    <Modal visible={visible[index]} width="1000" height="550" effect="fadeInUp" onClickAway={() => onCloseModal(index)}>
                         <pre dangerouslySetInnerHTML={{__html: query.graphql_query}}></pre>
                         <button type={'button'} className={"btn-secondary"} onClick={() => {
-                            onCloseModal();
+                            onCloseModal(index);
                         }}>Cancel</button>
                     </Modal>
                     <button onClick={() => {
