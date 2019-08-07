ac00e227daeeed6f9d67518d7edf64b9ed3c93e2
christopher daniel <christopherdaniel13397@gmail.com>
2019-08-07 23:04:26 +0530

    removed store in production

--- src/store.js
+++ src/store.js
@@ -22,7 +22,6 @@ const reducers = combineReducers({
 export default createStore(
     reducers,
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk)
     )
 );
